e0aec41d50a15ed08720cec9d65f8af47ddaf774
Dmytro Andreikiv <info@dma-mobile.dev>
2023-12-18 19:58:17 +0100

    AoC 2023, Day 7, Part 2

---
+++ 2023/Day_7/Part_2/solution.swift
@@ -0,0 +1,135 @@
+import Foundation
+import RegexBuilder
+
+class Solver {
+
+    private var input: [String] = []
+    init() {
+        loadInput()
+    }
+
+    private func loadInput() {
+        let url = URL(fileURLWithPath: "../input.txt")
+        do {
+            input = try String(contentsOf: url).components(separatedBy: "\n")
+        } catch {
+            print(error)
+        }
+    }
+
+    func printInput() {
+        for line in input {
+            print(line)
+        }
+    }
+
+    func solve() -> Int {
+        let hands = input.map {
+            let components = $0.components(separatedBy: " ")
+            return String(components[0])
+        }
+
+        let dict = input.reduce(into: [String: Int]()) { result, next in 
+            let components = next.components(separatedBy: " ")
+            let hand = components[0]
+            let bid = Int(components[1])!
+            result[hand] = bid
+        }
+
+        let cardStrength: [String: Int] = [
+            "A": 100-1, 
+            "K": 100-2, 
+            "Q": 100-3,
+            "T": 100-4, 
+            "9": 100-5,
+            "8": 100-6, 
+            "7": 100-7, 
+            "6": 100-8, 
+            "5": 100-9, 
+            "4": 100-10, 
+            "3": 100-11, 
+            "2": 100-12,
+            "J": 100-13
+        ]
+
+        let sortedHands = hands.sorted { hand1, hand2 in
+            let hand1Kind = self.handKind(hand1).rawValue
+            let hand2Kind = self.handKind(hand2).rawValue
+
+            if hand1Kind == hand2Kind {
+                let hand1Chars = hand1.map { String($0) }
+                let hand2Chars = hand2.map { String($0) }
+                for i in 0..<hand1Chars.count {
+                    let strength1 = cardStrength[hand1Chars[i]]!
+                    let strength2 = cardStrength[hand2Chars[i]]!
+                    if strength1 != strength2 {
+                        return strength1 < strength2
+                    }
+                }
+                return false
+            } else {
+                return hand1Kind < hand2Kind
+            }
+        }
+
+        var result = 0
+        for i in 0..<sortedHands.count {
+            let hand = sortedHands[i]
+            let bid = dict[hand]!
+            result += bid * (i + 1)
+        }
+
+        return result
+    }
+
+    func handKind(_ hand: String) -> HandKind {
+        var dict = hand.map { String($0) }.reduce(into: [String: Int]()) { result, label in
+            result[label, default: 0] += 1
+        }
+
+        let jCount = dict["J", default: 0]
+
+        if jCount > 0 {
+            var maxValue = 0
+            var maxValueKey = ""
+            for (key, value) in dict {
+                if key == "J" { continue }
+                if value > maxValue {
+                    maxValue = value
+                    maxValueKey = key
+                }
+            } 
+
+            dict[maxValueKey, default: 0] += jCount
+            dict.removeValue(forKey: "J")
+        }
+
+        let values = Array(dict.values).sorted()
+
+        switch values {
+            case [5]: return .fiveOfKind
+            case [1, 4]: return .fourOfKind
+            case [2, 3]: return .fullHouse
+            case [1, 1, 3]: return .threeOfKind
+            case [1, 2, 2]: return .twoPair
+            case [1, 1, 1, 2]: return .onePair
+            case [1, 1, 1, 1, 1]: return .highCard
+            default: return .unknown
+        }
+    }
+
+    enum HandKind: Int {
+        case fiveOfKind = 7
+        case fourOfKind = 6
+        case fullHouse = 5
+        case threeOfKind = 4
+        case twoPair = 3
+        case onePair = 2
+        case highCard = 1
+        case unknown = 0
+    }
+}
+
+let solver = Solver()
+solver.printInput()
+print(solver.solve())
